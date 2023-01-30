a78b5f53232d13dca08d4a1b1d762c63ec9ae764
Wollwolke <17803954+Wollwolke@users.noreply.github.com>
2023-01-30 15:45:17 +0100

    logger done

--- src/dtn_proxy/include/logger.hpp
+++ src/dtn_proxy/include/logger.hpp
@@ -2,37 +2,66 @@
 #include <functional>
 #include <iostream>
 
-#ifdef ROS_LOGGER
 #include "rclcpp/rclcpp.hpp"
-#endif
-
-namespace logger {
 
 class Logger {
-#ifdef ROS_LOGGER
-protected:
-    static rclcpp::Logger rosLogger;
-    static const bool useRos = true;
-
-public:
-    static void setLogger(rclcpp::Logger l);
-#elif
-protected:
-    static const bool useRos = false;
-#endif
-};
-
-class Debug : Logger {
 private:
+    rclcpp::Logger rosLogger;
+    std::string name;
+
+    class Base {
+    protected:
+        rclcpp::Logger rosLogger;
+        std::string namePrefix;
+        std::stringstream ss;
+
+    public:
+        Base(const rclcpp::Logger& rosLogger, const std::string& name);
+
+        template <typename T>
+        Base& operator<<(const T& value) {
+            ss << value;
+            return *this;
+        }
+    };
+
+    class Debug : public Base {
+    public:
+        Debug(const rclcpp::Logger& rosLogger, const std::string& name);
+        ~Debug();
+    };
+
+    class Info : public Base {
+    public:
+        Info(const rclcpp::Logger& rosLogger, const std::string& name);
+        ~Info();
+    };
+
+    class Warning : public Base {
+    public:
+        Warning(const rclcpp::Logger& rosLogger, const std::string& name);
+        ~Warning();
+    };
+
+    class Error : public Base {
+    public:
+        Error(const rclcpp::Logger& rosLogger, const std::string& name);
+        ~Error();
+    };
+
+    class Fatal : public Base {
+    public:
+        Fatal(const rclcpp::Logger& rosLogger, const std::string& name);
+        ~Fatal();
+    };
+
 public:
-    ~Debug();
+    Logger(const rclcpp::Logger& rosLogger, const std::string& name);
+    ~Logger();
 
-    template <class T>
-    Debug& operator<<(const T& v) {
-        RCLCPP_INFO_STREAM(rosLogger, "" << v);
-        std::cout << v;
-        return *this;
-    }
+    Debug DBG() { return Debug(rosLogger, name); }
+    Info INFO() { return Info(rosLogger, name); }
+    Warning WARN() { return Warning(rosLogger, name); }
+    Error ERR() { return Error(rosLogger, name); }
+    Fatal FATAL() { return Fatal(rosLogger, name); }
 };
-
-}  // namespace logger
--- src/dtn_proxy/src/logger.cpp
+++ src/dtn_proxy/src/logger.cpp
@@ -1,7 +1,46 @@
 #include <logger.hpp>
 
-using namespace logger;
+Logger::Logger(const rclcpp::Logger& rosLogger, const std::string& name)
+    : rosLogger(rosLogger), name(name) {}
+Logger::~Logger() {}
 
-Debug::~Debug() { std::cout << std::endl; }
+Logger::Base::Base(const rclcpp::Logger& rosLogger, const std::string& name)
+    : rosLogger(rosLogger) {
+    namePrefix = "[" + name + "]: ";
+}
 
-void Logger::setLogger(rclcpp::Logger l) { rosLogger = l; }
+Logger::Debug::Debug(const rclcpp::Logger& rosLogger, const std::string& name)
+    : Logger::Base(rosLogger, name) {}
+Logger::Debug::~Debug() {
+    std::string str = namePrefix + ss.str();
+    if (!str.empty()) RCLCPP_DEBUG_STREAM(rosLogger, str);
+}
+
+Logger::Info::Info(const rclcpp::Logger& rosLogger, const std::string& name)
+    : Logger::Base(rosLogger, name) {}
+Logger::Info::~Info() {
+    std::string str = namePrefix + ss.str();
+    if (!str.empty()) RCLCPP_INFO_STREAM(rosLogger, str);
+}
+
+Logger::Warning::Warning(const rclcpp::Logger& rosLogger,
+                         const std::string& name)
+    : Logger::Base(rosLogger, name) {}
+Logger::Warning::~Warning() {
+    std::string str = namePrefix + ss.str();
+    if (!str.empty()) RCLCPP_WARN_STREAM(rosLogger, str);
+}
+
+Logger::Error::Error(const rclcpp::Logger& rosLogger, const std::string& name)
+    : Logger::Base(rosLogger, name) {}
+Logger::Error::~Error() {
+    std::string str = namePrefix + ss.str();
+    if (!str.empty()) RCLCPP_ERROR_STREAM(rosLogger, str);
+}
+
+Logger::Fatal::Fatal(const rclcpp::Logger& rosLogger, const std::string& name)
+    : Logger::Base(rosLogger, name) {}
+Logger::Fatal::~Fatal() {
+    std::string str = namePrefix + ss.str();
+    if (!str.empty()) RCLCPP_FATAL_STREAM(rosLogger, str);
+}
