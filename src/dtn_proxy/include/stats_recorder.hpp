b63705aab8b3d1164a9e9a0a30595270c873a892
Wollwolke <17803954+Wollwolke@users.noreply.github.com>
2023-03-30 00:26:44 +0200

    reworked stats recorder

--- src/dtn_proxy/include/stats_recorder.hpp
+++ src/dtn_proxy/include/stats_recorder.hpp
@@ -1,10 +1,7 @@
 #pragma once
 
-#include <filesystem>
-#include <map>
-#include <nlohmann/json.hpp>
+#include <fstream>
 #include <string>
-#include <utility>
 
 #include "ros/dtn_msg_type.hpp"
 
@@ -12,39 +9,8 @@ namespace dtnproxy {
 
 class StatsRecorder {
 private:
-    using Entry = struct Entry {
-        using InternalEntry = struct InternalEntry {
-            uint32_t size{0};
-            uint32_t count{0};
-            NLOHMANN_DEFINE_TYPE_INTRUSIVE(InternalEntry, size, count)
-        };
-
-        std::string msgType;
-        InternalEntry rosEntry;
-        InternalEntry dtnEntry;
-
-        Entry(const std::string& msgType) : msgType(msgType) {}
-
-        // ! Don't call this ctor
-        // Need it for map initialisation?
-        Entry() = default;
-
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(Entry, msgType, rosEntry, dtnEntry)
-    };
-
-    using serviceStats_t = std::map<std::string, std::map<std::string, Entry>>;
-    static constexpr auto REQUEST = "request";
-    static constexpr auto RESPONSE = "response";
-
-    std::map<std::string, Entry> subStats;
-    std::map<std::string, Entry> pubStats;
-    serviceStats_t serviceStats;
-    serviceStats_t clientStats;
-    std::filesystem::path statsDir;
-
-    void incrementEntry(Entry::InternalEntry& entry, uint32_t size);
-    auto initServiceStats(serviceStats_t& stats, const std::string& topic,
-                          const std::string& msgType);
+    std::ofstream file;
+    std::string timestamp();
 
 public:
     StatsRecorder(const std::string& statsDir);
@@ -58,9 +24,6 @@ public:
                      ros::DtnMsgType rosType);
     void dtnSent(const std::string& endpoint, const std::string& msgType, uint32_t size,
                  ros::DtnMsgType rosType);
-    void saveToDisk();
-
-    friend void to_json(nlohmann::json& j, const StatsRecorder& obj);
 };
 
 }  // namespace dtnproxy
--- src/dtn_proxy/src/stats_recorder.cpp
+++ src/dtn_proxy/src/stats_recorder.cpp
@@ -1,120 +1,35 @@
 #include "stats_recorder.hpp"
 
 #include <ctime>
+#include <filesystem>
 #include <fstream>
-#include <iostream>
+#include <iomanip>
+#include <sstream>
 
 #include "ros/dtn_msg_type.hpp"
 
 namespace dtnproxy {
 
-auto StatsRecorder::initServiceStats(serviceStats_t& stats, const std::string& topic,
-                                     const std::string& msgType) {
-    std::map<std::string, Entry> tmp = {{REQUEST, Entry(msgType)}, {RESPONSE, Entry(msgType)}};
-    return stats.emplace(topic, tmp).first;
+std::string StatsRecorder::timestamp() {
+    using Clock = std::chrono::system_clock;
+
+    auto now = Clock::now();
+    auto seconds = std::chrono::time_point_cast<std::chrono::seconds>(now);
+    auto fraction = now - seconds;
+    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(fraction);
+
+    time_t cnow = Clock::to_time_t(now);
+
+    std::stringstream timeStream;
+    timeStream << std::put_time(std::localtime(&cnow), "%FT%T.");
+    timeStream << milliseconds.count();
+    timeStream << std::put_time(std::localtime(&cnow), "%z") << ";";
+
+    return timeStream.str();
 }
 
-void StatsRecorder::incrementEntry(Entry::InternalEntry& entry, uint32_t size) {
-    entry.count += 1;
-    if (0 == entry.size) {
-        entry.size = size;
-    } else if (size != entry.size) {
-        std::cout << "Stats: Entry size changed!!!" << std::endl;
-    }
-}
-
-StatsRecorder::StatsRecorder(const std::string& statsDir) { this->statsDir.append(statsDir); }
-
-StatsRecorder::~StatsRecorder() { saveToDisk(); }
-
-// TODO: refactor this...
-
-void StatsRecorder::rosReceived(const std::string& topic, const std::string& msgType, uint32_t size,
-                                ros::DtnMsgType rosType) {
-    switch (rosType) {
-        case ros::DtnMsgType::TOPIC: {
-            auto result = subStats.emplace(topic, msgType);
-            incrementEntry(result.first->second.rosEntry, size);
-        } break;
-        case ros::DtnMsgType::REQUEST: {
-            auto result = initServiceStats(serviceStats, topic, msgType);
-            incrementEntry(result->second[REQUEST].rosEntry, size);
-        } break;
-        case ros::DtnMsgType::RESPONSE: {
-            auto result = initServiceStats(clientStats, topic, msgType);
-            incrementEntry(result->second[RESPONSE].rosEntry, size);
-        } break;
-        case ros::DtnMsgType::INVALID:
-        default:
-            return;
-    }
-}
-
-void StatsRecorder::rosSent(const std::string& topic, const std::string& msgType, uint32_t size,
-                            ros::DtnMsgType rosType) {
-    switch (rosType) {
-        case ros::DtnMsgType::TOPIC: {
-            auto result = pubStats.emplace(topic, msgType);
-            incrementEntry(result.first->second.rosEntry, size);
-        } break;
-        case ros::DtnMsgType::REQUEST: {
-            auto result = initServiceStats(clientStats, topic, msgType);
-            incrementEntry(result->second[REQUEST].rosEntry, size);
-        } break;
-        case ros::DtnMsgType::RESPONSE: {
-            auto result = initServiceStats(serviceStats, topic, msgType);
-            incrementEntry(result->second[RESPONSE].rosEntry, size);
-        } break;
-        case ros::DtnMsgType::INVALID:
-        default:
-            return;
-    }
-}
-
-void StatsRecorder::dtnReceived(const std::string& endpoint, const std::string& msgType,
-                                uint32_t size, ros::DtnMsgType rosType) {
-    switch (rosType) {
-        case ros::DtnMsgType::TOPIC: {
-            auto result = pubStats.emplace(endpoint, msgType);
-            incrementEntry(result.first->second.dtnEntry, size);
-        } break;
-        case ros::DtnMsgType::REQUEST: {
-            auto result = initServiceStats(clientStats, endpoint, msgType);
-            incrementEntry(result->second[REQUEST].dtnEntry, size);
-        } break;
-        case ros::DtnMsgType::RESPONSE: {
-            auto result = initServiceStats(serviceStats, endpoint, msgType);
-            incrementEntry(result->second[RESPONSE].dtnEntry, size);
-        } break;
-        case ros::DtnMsgType::INVALID:
-        default:
-            return;
-    }
-}
-
-void StatsRecorder::dtnSent(const std::string& endpoint, const std::string& msgType, uint32_t size,
-                            ros::DtnMsgType rosType) {
-    switch (rosType) {
-        case ros::DtnMsgType::TOPIC: {
-            auto result = subStats.emplace(endpoint, msgType);
-            incrementEntry(result.first->second.dtnEntry, size);
-        } break;
-        case ros::DtnMsgType::REQUEST: {
-            auto result = initServiceStats(serviceStats, endpoint, msgType);
-            incrementEntry(result->second[REQUEST].dtnEntry, size);
-        } break;
-        case ros::DtnMsgType::RESPONSE: {
-            auto result = initServiceStats(clientStats, endpoint, msgType);
-            incrementEntry(result->second[RESPONSE].dtnEntry, size);
-        } break;
-        case ros::DtnMsgType::INVALID:
-        default:
-            return;
-    }
-}
-
-void StatsRecorder::saveToDisk() {
-    nlohmann::json jStats = *this;
+StatsRecorder::StatsRecorder(const std::string& statsDir) {
+    std::filesystem::path statsPath(statsDir);
 
     auto time = std::time(nullptr);
 
@@ -122,18 +37,120 @@ void StatsRecorder::saveToDisk() {
     char fileName[lenOfDateString];
     std::strftime(fileName, lenOfDateString, "%Y-%m-%d_%H-%M-%S", std::localtime(&time));
 
-    auto path = statsDir;
-    path.append(fileName).replace_extension(".json");
+    auto filePath = statsPath;
+    filePath.append(fileName).replace_extension(".log");
 
     // TODO: handle file errors
-    std::filesystem::create_directories(statsDir);
-    std::ofstream ofs(path.string());
-    ofs << std::setw(4) << jStats;
+    std::filesystem::create_directories(statsPath);
+    file.open(filePath);
 }
 
-void to_json(nlohmann::json& j, const StatsRecorder& obj) {
-    j = nlohmann::json{{"topics", {{"subscriber", obj.subStats}, {"publisher", obj.pubStats}}},
-                       {"services", {{"client", obj.clientStats}, {"server", obj.serviceStats}}}};
+StatsRecorder::~StatsRecorder() { file.close(); }
+
+// TODO: refactor this...
+
+void StatsRecorder::rosReceived(const std::string& topic, const std::string& msgType, uint32_t size,
+                                ros::DtnMsgType rosType) {
+    std::stringstream logMsg;
+    logMsg << timestamp() << "ROS;";
+
+    switch (rosType) {
+        case ros::DtnMsgType::TOPIC: {
+            logMsg << "TOPIC;"
+                   << "SUB;";
+        } break;
+        case ros::DtnMsgType::REQUEST: {
+            logMsg << "SERVER;"
+                   << "REQUEST;";
+        } break;
+        case ros::DtnMsgType::RESPONSE: {
+            logMsg << "CLIENT;"
+                   << "RESPONSE;";
+        } break;
+        case ros::DtnMsgType::INVALID:
+        default:
+            return;
+    }
+    logMsg << topic << ";" << msgType << ";" << size;
+    file << logMsg.rdbuf() << std::endl;
+}
+
+void StatsRecorder::rosSent(const std::string& topic, const std::string& msgType, uint32_t size,
+                            ros::DtnMsgType rosType) {
+    std::stringstream logMsg;
+    logMsg << timestamp() << "ROS;";
+
+    switch (rosType) {
+        case ros::DtnMsgType::TOPIC: {
+            logMsg << "TOPIC;"
+                   << "PUB;";
+        } break;
+        case ros::DtnMsgType::REQUEST: {
+            logMsg << "CLIENT;"
+                   << "REQUEST;";
+        } break;
+        case ros::DtnMsgType::RESPONSE: {
+            logMsg << "SERVER;"
+                   << "RESPONSE;";
+        } break;
+        case ros::DtnMsgType::INVALID:
+        default:
+            return;
+    }
+    logMsg << topic << ";" << msgType << ";" << size;
+    file << logMsg.rdbuf() << std::endl;
+}
+
+void StatsRecorder::dtnReceived(const std::string& endpoint, const std::string& msgType,
+                                uint32_t size, ros::DtnMsgType rosType) {
+    std::stringstream logMsg;
+    logMsg << timestamp() << "DTN;";
+
+    switch (rosType) {
+        case ros::DtnMsgType::TOPIC: {
+            logMsg << "TOPIC;"
+                   << "PUB;";
+        } break;
+        case ros::DtnMsgType::REQUEST: {
+            logMsg << "CLIENT;"
+                   << "REQUEST;";
+        } break;
+        case ros::DtnMsgType::RESPONSE: {
+            logMsg << "SERVER;"
+                   << "RESPONSE;";
+        } break;
+        case ros::DtnMsgType::INVALID:
+        default:
+            return;
+    }
+    logMsg << endpoint << ";" << msgType << ";" << size;
+    file << logMsg.rdbuf() << std::endl;
+}
+
+void StatsRecorder::dtnSent(const std::string& endpoint, const std::string& msgType, uint32_t size,
+                            ros::DtnMsgType rosType) {
+    std::stringstream logMsg;
+    logMsg << timestamp() << "DTN;";
+
+    switch (rosType) {
+        case ros::DtnMsgType::TOPIC: {
+            logMsg << "TOPIC;"
+                   << "SUB;";
+        } break;
+        case ros::DtnMsgType::REQUEST: {
+            logMsg << "SERVER;"
+                   << "REQUEST;";
+        } break;
+        case ros::DtnMsgType::RESPONSE: {
+            logMsg << "CLIENT;"
+                   << "RESPONSE;";
+        } break;
+        case ros::DtnMsgType::INVALID:
+        default:
+            return;
+    }
+    logMsg << endpoint << ";" << msgType << ";" << size;
+    file << logMsg.rdbuf() << std::endl;
 }
 
 }  // namespace dtnproxy
