17cae1a7829b5ae49d46964bba4a7419f53e3541
Wollwolke <17803954+Wollwolke@users.noreply.github.com>
2023-04-13 19:17:56 +0200

    sim: player can skip at start

--- src/dtn_sim/scripts/playRosbag.py
+++ src/dtn_sim/scripts/playRosbag.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-"""Custom Rosbag player, that replaces timestamps with the current RosTime."""
+"""Custom Rosbag player, that replaces timestamps with the current RosTime.
+May not work for tf tree msgs"""
 
 import argparse
 import threading
@@ -24,9 +25,11 @@ class TimelyRosbagPlayer(Node):
         self.pubs = {}
         self.file = args.input
         self.loop = args.loop
+        self.skip = args.skip
 
     def get_pub(self, topic, type):
         if not topic in self.pubs:
+            self.get_logger().info(f"Publishing on topic: {topic}")
             if "/tf_static" == topic:
                 self.pubs[topic] = self.create_publisher(
                     type,
@@ -44,9 +47,9 @@ class TimelyRosbagPlayer(Node):
         self.th.start()
 
     def play(self):
-        while self.loop:
+        while True:
             timeDiff = None
-            for topic, msg_type, msg, timestamp in read_messages(self.file):
+            for topic, msg_type, msg, timestamp in read_messages(self.file, int(self.skip * 1e9)):
                 pub = self.get_pub(topic, msg_type)
 
                 if None == timeDiff:
@@ -76,9 +79,24 @@ class TimelyRosbagPlayer(Node):
                 if not rclpy.ok():
                     return
                 pub.publish(msg)
+            if not self.loop:
+                return
 
 
-def read_messages(input_bag: str):
+def read_messages(input_bag: str, skip_nsec: int):
+    if 0!= skip_nsec:
+        reader = rosbag2_py.SequentialReader()
+        reader.open(
+            rosbag2_py.StorageOptions(uri=input_bag, storage_id="mcap"),
+            rosbag2_py.ConverterOptions(
+                input_serialization_format="cdr", output_serialization_format="cdr"
+            ),
+        )
+        if reader.has_next():
+            _, _, timestamp = reader.read_next()
+            skip_nsec += timestamp
+        del reader
+
     reader = rosbag2_py.SequentialReader()
     reader.open(
         rosbag2_py.StorageOptions(uri=input_bag, storage_id="mcap"),
@@ -88,6 +106,9 @@ def read_messages(input_bag: str):
     )
 
     topic_types = reader.get_all_topics_and_types()
+
+    if 0 != skip_nsec:
+        reader.seek(skip_nsec)
 
     def typename(topic_name):
         for topic_type in topic_types:
@@ -104,16 +125,25 @@ def read_messages(input_bag: str):
 
 
 def main():
-    parser = argparse.ArgumentParser(description=__doc__)
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     parser.add_argument(
-        "input", help="input bag path (folder or filepath) to read from"
+        "input", help="Input bag path (folder or filepath) to read from"
     )
     parser.add_argument(
         "--loop",
-        help="restarts the bag when the end is reached",
+        help="Restarts the bag when the end is reached",
         dest="loop",
         default=False,
         action="store_true",
+    )
+    parser.add_argument(
+        "--start-offset",
+        help="Start the playback this many seconds into the bag file.",
+        dest="skip",
+        type=int,
+        default=0,
     )
     args = parser.parse_args()
 
